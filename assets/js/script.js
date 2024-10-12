cefca4daaafed05521cc5cc4faf2ec43e7df91e6
Charles Adrian <163716236+psykwicc@users.noreply.github.com>
2024-10-12 14:39:54 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,6 +157,8 @@ ScrollReveal().reveal('', { origin: 'top' });
 
 ScrollReveal().reveal('.navBarContainer, .about, .skills, .education, .project, .experience, .skills__tit, .p1, .p2, .p3, .p4, .p5, .p6, .p7, .p8, .skills-box, .skills__icon, .skills__desc, .project__item', { origin: 'bottom' });
 
-ScrollReveal().reveal('.header__name, .header__program, .header__bio, .nav__links, .social__icons', { origin: 'left' });
+ScrollReveal().reveal('.header__, .header__bio, .nav__links, .social__icons', { origin: 'left' });
 
 ScrollReveal().reveal('', { origin: 'right' });
+
+

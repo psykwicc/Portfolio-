0728c70cabb64f6a948d905aa276b99afe454284
Charles Adrian <163716236+psykwicc@users.noreply.github.com>
2024-10-13 10:52:19 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,3 +169,33 @@ ScrollReveal().reveal('.project__item', {
         el.style.transition = 'all 0.5s';  // Ensure hover effects kick in after the reveal animation
     }
 });
+
+
+let isOriginal = true;  // Track the state
+
+function toggleIcon() {
+    const iconSvg = document.getElementById('iconSvg');
+
+    if (isOriginal) {
+        // Switch to the second SVG
+        iconSvg.innerHTML = `
+      <path d="M21.5 14.0784C20.3003 14.7189 18.9301 15.0821 17.4751 15.0821C12.7491 15.0821 8.91792 11.2509 8.91792 6.52485C8.91792 5.06986 9.28105 3.69968 9.92163 2.5C5.66765 3.49698 2.5 7.31513 2.5 11.8731C2.5 17.1899 6.8101 21.5 12.1269 21.5C16.6849 21.5 20.503 18.3324 21.5 14.0784Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    `;
+    } else {
+        // Switch back to the original SVG
+        iconSvg.innerHTML = `
+      
+    <path
+              d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z"
+              stroke="currentColor" stroke-width="1.5" />
+            <path
+              d="M12 2V3.5M12 20.5V22M19.0708 19.0713L18.0101 18.0106M5.98926 5.98926L4.9286 4.9286M22 12H20.5M3.5 12H2M19.0713 4.92871L18.0106 5.98937M5.98975 18.0107L4.92909 19.0714"
+              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+          
+
+    `;
+    }
+
+    // Toggle the state
+    isOriginal = !isOriginal;
+}
